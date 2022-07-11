c02d86207b8b463825e6fabfcedac84727c7954f
Stefan Wetzig <stefanwetzig@yahoo.de>
2022-07-12 00:41:33 +0200

    added invitelink

--- src/game/game.component.jsx
+++ src/game/game.component.jsx
@@ -19,7 +19,7 @@ class GameComponent extends Component {
         this.state = {
             gameId: JSON.parse(localStorage.getItem("auth")).gameId,
             me: JSON.parse(localStorage.getItem("auth")).player,
-            inviteLink: "http://" + serverConfig.host + ":3000/joingame/" + JSON.parse(localStorage.getItem("auth")).gameId,
+            inviteLink: serverConfig.hostfe + "/joingame/" + JSON.parse(localStorage.getItem("auth")).gameId,
 
             //updater
             isInited: false,
